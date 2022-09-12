b6d73405f725bcfcdd3fa89a4be1b270594b120e
Wagner <wagneralbuquerque2010@gmail.com>
2022-09-12 10:52:36 -0300

    Calcul Windows

--- js/calcula.js
+++ js/calcula.js
@@ -1,10 +1,32 @@
-var digitos = document.querySelector(".digitos");
-var digit = digitos.querySelectorAll(".digit");
-console.log(digit)
+var digitos = document.querySelectorAll(".digit");
+var visor = document.querySelector(".visor");
 
 
+function tolimit(){
 
 
+    visor.textContent = visor.textContent.substring(0,10);
+
+}
+
+function insere(num){
+
+    
+    visor.textContent += num;
+    visor.textContent = visor.textContent.substring(0,15);
+
+}
+
+function limpa(){
+    visor.textContent = ("");
+
+}
+
+function calcular(){
+    visor.textContent = eval(visor.textContent);
+}
+function backspace(){
+}
 
 
-
+console.log(visor)
